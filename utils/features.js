2b9331bcb4f614d6d2f31f44d803f493bdf0f21c
SYED175 <syedismailgp@gmail.com>
2024-10-05 13:49:40 +0530

    Update cookie response by adding sameSite and secure params

--- utils/features.js
+++ utils/features.js
@@ -2,14 +2,23 @@ import jwt from "jsonwebtoken";
 
 export const sendCookies = (admin, statusCode, message, res) => {
   const token = jwt.sign({ _id: admin._id }, process.env.JWT_SECRET);
+  const adminInfo = {
+    _id: admin._id,
+    name: admin.name,
+    email: admin.email,
+    // Add any other non-sensitive fields you want to include
+  };
   res
     .status(statusCode)
     .cookie("token", token, {
       httpOnly: true,
-      maxAge: 15 * 60 * 1000,
+      maxAge: 1000 * 60 * 60,
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
       success: true,
       message,
+      admin: adminInfo,
     });
 };
