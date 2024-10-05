8acdf9f98a18ea625a82ab2f91e69dbd9e076288
SYED175 <syedismailgp@gmail.com>
2024-10-05 13:50:08 +0530

    Update routes for student and admin

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ import { isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/new", registerAdmin);
+router.post("/new", isAuthenticated, registerAdmin);
 router.post("/login", adminLogin);
 router.get("/logout", isAuthenticated, adminLogout);
 router.get("/me", isAuthenticated, getMyProfile);
--- routes/student.js
+++ routes/student.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   allStudents,
+  deleteStudentById,
   getStudentsByPagination,
   registerStudent,
 } from "../controllers/student.js";
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/new", registerStudent);
 router.get("/all", isAuthenticated, allStudents);
 router.get("/paginStudents", isAuthenticated, getStudentsByPagination);
+router.delete("/:id", isAuthenticated, deleteStudentById);
 
 export default router;
