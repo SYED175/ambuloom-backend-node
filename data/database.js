0d13982a5fbcadadfad35d63e7f59c84b041bd44
SYED175 <syedismailgp@gmail.com>
2024-10-05 21:40:04 +0530

    Remove config.env file from tracking and to .gitignore

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect("mongodb://localhost:27017", {
+    .connect(process.env.MONGO_URI, {
       dbName: "ambuloom-backend",
     })
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
